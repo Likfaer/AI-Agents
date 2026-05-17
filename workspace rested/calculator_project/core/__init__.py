66d6f6ff263d085dfce4f2b354cb47f1703f97ef
Likfaer <minal01@mail.ru>
2026-05-17 22:59:30 +0300

    Delete workspace rested directory

--- workspace rested/calculator_project/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core модуль калькулятора - основные компоненты."""
-from .exceptions import InvalidInputError, CalculationError
-from .math_ops import MathOperations
-from .calculator import Calculator
-
-__all__ = ['Calculator', 'MathOperations', 'InvalidInputError', 'CalculationError']
